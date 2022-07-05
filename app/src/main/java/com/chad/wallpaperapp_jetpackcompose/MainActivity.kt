357c350bf46e0609db9b915adf510b5267284dde
keithchad <keithchad10@outlook.com>
2022-07-05 20:50:15 +0300

    initial commit

---
+++ app/src/main/java/com/chad/wallpaperapp_jetpackcompose/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.chad.wallpaperapp_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.chad.wallpaperapp_jetpackcompose.ui.theme.WallpaperappjetpackcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WallpaperappjetpackcomposeTheme {
+                Content()
+            }
+        }
+    }
+}
+
+@Composable
+fun Content() {}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WallpaperappjetpackcomposeTheme {
+        Content()
+    }
+}
